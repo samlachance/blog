e292bced2908e8661e952dc68d98d71cc52275d0
Sam Lachance <slachance@gmail.com>
2016-08-18 16:43:51 -0400

    added redcarpet

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,9 @@ module ApplicationHelper
   def posts_for_sidebar
     Post.all.order('created_at DESC')
   end
+
+  def markdown(text)
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
+    return markdown.render(text).html_safe
+  end
 end
