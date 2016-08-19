ac609f0f8b5e7fe25b6d509a83dea0345539443b
Sam Lachance <slachance@gmail.com>
2016-08-19 10:47:02 -0400

    created destroy method

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160818220029) do
+ActiveRecord::Schema.define(version: 20160819143527) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20160818220029) do
     t.datetime "updated_at",                          null: false
     t.string   "first_name"
     t.string   "last_name"
+    t.boolean  "is_admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
